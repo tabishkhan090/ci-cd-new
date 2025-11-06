5eecf823217e3a34fedbb094da7719dec233a577
MD TABISH <tabishrazakhan92@gmail.com>
2025-11-07 03:02:11 +0530

    manually deploying

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,11 @@
-
 import { client } from "@repo/db/client";
 
 export default async function Home() {
 const user = await client.user.findFirst();
   return (
     <div>
-      First name: 
-      {user?.username}
-      password: 
-      {user?.password}
+      <div>First name: {user?.username}</div>
+      <div>Password: {user?.password}</div>
     </div>
   );
 }
