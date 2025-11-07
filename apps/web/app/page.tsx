4d17f6cbecd3c25248ab08989713aa41bd3f0b36
MD TABISH <tabishrazakhan92@gmail.com>
2025-11-07 20:02:15 +0530

    testing production branch 2

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 const user = await client.user.findFirst();
   return (
     <div>
-      First name: 
+      First name Tabish: 
       {user?.username}
       password: 
       {user?.password}
